48b243b0309e5e338ddf68d65e647146d4a11ea4
CrackjY <yassine.latti@gmail.com>
2022-09-18 02:05:07 +0200

    favorit avec ajax

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,5 +1,25 @@
-$.ajax({
-    url: a_cross_domain_url,
-    success: function() {
-    }
+$(function () {
+    $('body').on('click', '.btn-favorite', function(e) {
+        e.preventDefault();
+
+        const isFavorite = $(this);
+        const route = isFavorite.data('href');
+
+        console.log(isFavorite.children().attr('class'));
+
+        $.ajax({
+            url: route,
+            success: function (response) {
+                if (isFavorite.children().hasClass('bi-hand-thumbs-up-fill')) {
+                    isFavorite.children().removeClass('bi-hand-thumbs-up-fill')
+                    isFavorite.children().addClass('bi-hand-thumbs-up')
+                } else {
+                    isFavorite.children().removeClass('bi-hand-thumbs-up')
+                    isFavorite.children().addClass('bi-hand-thumbs-up-fill')
+                }
+            }
+        })
+    })
 });
+
+
